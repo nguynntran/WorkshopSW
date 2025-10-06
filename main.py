9ec8c599fcfea9cf5e50593bed848ba7de5513ac
Trần Nguyên <nguyen3503@tdkn3.local>
2025-10-06 15:10:46 +0300

    New MAIN

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ import pygame
 from checkers.board import Board
 from checkers.constant import Cons
 
+FPS = 60
+screen = pygame.display.set_mode((Cons.COLS * Cons.SQUARE_SIZE, Cons.ROWS * Cons.SQUARE_SIZE))
+pygame.display.set_caption("WOLFS AND SHEEP")
+
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((Cons.COLS * Cons.SQUARE_SIZE, Cons.ROWS * Cons.SQUARE_SIZE))
-    pygame.display.set_caption("Checkers Game")
-
+    
     board = Board()
     clock = pygame.time.Clock()
     running = True
 
     while running:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
         board.draw_board(screen)
         pygame.display.flip()
-        clock.tick(60)
+        
 
     pygame.quit()
 
