fe28869a31548f34d3f6c780d12f64a207697fbe
Trần Nguyên <nguyen3503@tdkn3.local>
2025-10-27 12:27:28 +0300

    new board

--- checkers/wolfandsheep/board.py
+++ checkers/wolfandsheep/board.py
@@ -10,7 +10,7 @@ class Board(BaseBoard):
         self.setup_initial_pieces()
     
     def setup_initial_pieces(self):
-        """Setup initial pieces for wolves and sheep game"""
+        # Setup initial pieces for wolves and sheep game
         # Place wolves on top row at dark squares
         for col in [1, 3, 5, 7]:
             self.board[0][col] = WolfPiece(0, col)
